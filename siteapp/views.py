f0dc8ae33612cfecf0d2ee1d26026dbad8b58553
Sam Contarino <sdcontarino@gmail.com>
2020-01-29 15:22:11 +1000

    First commit - website template

---
+++ siteapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'siteapp/home.html', {'title': 'Home'})
+
+
+def about(request):
+    return render(request, 'siteapp/about.html', {'title': 'About'})
---
+++ staticfiles/js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var $burgers = getAll('.burger');
+
+  if ($burgers.length > 0) {
+    $burgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+
+  // Functions
+  function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+  }
+});
